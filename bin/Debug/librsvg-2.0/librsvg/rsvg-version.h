7535a5ed3a2ae83e33c5e250ddd774bfec2b9c4c
Gautham Ganesh <gauthamganesh02@gmail.com>
2025-10-14 21:43:27 +0530

    #2nd_commit

---
+++ bin/Debug/librsvg-2.0/librsvg/rsvg-version.h
@@ -0,0 +1,13 @@
+#if !defined (__RSVG_RSVG_H_INSIDE__) && !defined (RSVG_COMPILATION)
+#warning "Including <librsvg/rsvg-version.h> directly is deprecated."
+#endif
+
+#ifndef RSVG_VERSION_H
+#define RSVG_VERSION_H
+
+#define LIBRSVG_MAJOR_VERSION (2)
+#define LIBRSVG_MINOR_VERSION (61)
+#define LIBRSVG_MICRO_VERSION (1)
+#define LIBRSVG_VERSION "2.61.1"
+
+#endif
